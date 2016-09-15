b766a2e4cebe8322b7084f1b5fa537c06dadaa69
Rameshraju7 <mallelaramesh5@gmail.com>
2016-09-15 11:23:39 +0530

    SingleDecleration

---
+++ SingleDecleration.java
@@ -0,0 +1,13 @@
+
+public class SingleDecleration {
+
+	public static void main(String[] args) {
+		
+		int a,b=4,c;
+		
+		// TODO Auto-generated method stub
+		System.out.println("Value of b = "+b);
+
+	}
+
+}
